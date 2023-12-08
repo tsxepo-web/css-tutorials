87888eb6b0154b3af6026f15cff04b1e05630798
tsxepo <tsxepo@gmail.com>
2023-12-08 13:45:10 +0200

    almost

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# TestCss
+# Front-end Style Guide
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
+## Layout
 
-## Development server
+The designs were created to the following widths:
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+- Mobile: 375px
+- Desktop: 1440px
 
-## Code scaffolding
+## Colors
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+### Neutral
 
-## Build
+- Almost White: hsl(0, 0%, 98%)
+- Medium Gray: hsl(0, 0%, 41%)
+- Almost Black: hsl(0, 0%, 8%)
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+## Typography
 
-## Running unit tests
+### Body Copy
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+- Font size (paragraph): 18px
 
-## Running end-to-end tests
+### Font
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+- Family: [Epilogue](https://fonts.google.com/specimen/Epilogue)
+- Weights: 500, 700
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-css';
+  isDropdownOpen = false;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: Event) {
+    const isDropdownButton = (event.target as HTMLElement).matches(
+      '[data-dropdown-button]'
+    );
+
+    if (
+      !isDropdownButton &&
+      (event.target as HTMLElement).closest('[data-dropdown]') !== null
+    ) {
+      return;
+    }
+
+    let currentDropdown: HTMLElement | null;
+
+    if (isDropdownButton) {
+      currentDropdown = (event.target as HTMLElement).closest(
+        '[data-dropdown]'
+      );
+      currentDropdown!.classList.toggle('active');
+      this.isDropdownOpen = currentDropdown!.classList.contains('active');
+    }
+
+    document
+      .querySelectorAll('[data-dropdown].active')
+      .forEach((dropdown: any) => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+      });
+  }
+
+  toggleDropdown() {
+    const dropdownElement =
+      this.el.nativeElement.querySelector('[data-dropdown]');
+    dropdownElement.classList.toggle('active');
+    this.isDropdownOpen = dropdownElement.classList.contains('active');
+  }
 }
